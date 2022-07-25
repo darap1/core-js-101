eb8f97869e1dc8c996317f0988292cbe5d521c8a
darap1 <46171266+darap1@users.noreply.github.com>
2022-07-25 23:50:53 +0200

    feat: refactor Rectangle with space

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -19,14 +19,8 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  return {
-    width,
-    height,
-    getArea() {
-      return this.width * this.height;
-    },
-  };
+function Rectangle(/* width, height */) {
+  throw new Error('Not implemented');
 }
 
 
